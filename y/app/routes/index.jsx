54d77555bdd2d4e844dcee61741d1c7dc5f7168f
anssimv <anssi.vilppunen@panic.fi>
2022-08-30 09:30:43 +0300

    dokumentaation demo

--- y/app/routes/index.jsx
+++ y/app/routes/index.jsx
@@ -9,6 +9,16 @@ export default function Index() {
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
+
+
+<div className="mx-auto mt-16 max-w-7xl text-center">
+  <Link
+    to="/posts"
+    className="text-xl text-blue-600 underline"
+  >
+    Blog Posts
+  </Link>
+</div>
             15m Quickstart Blog Tutorial
           </a>
         </li>
