1f007f23e2533c218f5acda16f6124a7ead34c89
anssimv <anssi.vilppunen@panic.fi>
2022-08-30 09:42:13 +0300

    kommentit poistettu

--- y/app/routes/index.jsx
+++ y/app/routes/index.jsx
@@ -1,8 +1,8 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      
-     {/*  <h1>Welcome to Remix</h1>
+
+       <h1>Welcome to Remix</h1>
       <ul>
         <li>
           <a
@@ -28,6 +28,6 @@ export default function Index() {
           </a>
         </li>
       </ul>
-    </div> */}
+    </div> 
   );
 }
