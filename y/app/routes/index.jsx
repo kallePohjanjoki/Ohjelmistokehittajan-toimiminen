985558bd2d9e8ca9477611171bf7277dd209b6c1
anssimv <anssi.vilppunen@panic.fi>
2022-08-30 13:18:29 +0300

    kopioitu demon pohja

--- y/app/routes/index.jsx
+++ y/app/routes/index.jsx
@@ -1,8 +1,33 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getHinnat } from "~/api.server";
+
+
+
+export const loader = async () => {
+  const hinnat = await getHinnat();
+  return json(hinnat));
+};
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
 
        <h1>Welcome to Remix</h1>
+
+       <ul>
+        {ostoslista.map((ostos, index) => {
+          const nimi = ostos.nimi;
+          if (ostos.hankittu) {
+            return (
+              <li key={index}>
+                <s>{nimi}</s>
+              </li>
+            );
+          }
+          return <li key={index}>{nimi}</li>;
+        })}
+      </ul>
      
     </div> 
   );
