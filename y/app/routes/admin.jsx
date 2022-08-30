ec4faa3ddb288d7952e3da8320370d94de430f27
anssimv <anssi.vilppunen@panic.fi>
2022-08-30 10:47:46 +0300

    teskstiboksi

--- y/app/routes/admin.jsx
+++ y/app/routes/admin.jsx
@@ -3,6 +3,21 @@ export default function Index() {
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
 
        <h1>Hallinta</h1>
+
+       <input type="text" id="myText" value="Some text...">
+
+        <p>Click the "Try it" button to get the text in the text field.</p>
+
+        <button onclick="myFunction()">Try it</button>
+
+        <p id="demo"></p>
+
+        <script>
+        function myFunction() {
+        var x = document.getElementById("myText").value;
+        document.getElementById("demo").innerHTML = x;
+        }
+        </script>
       
     </div> 
   );
