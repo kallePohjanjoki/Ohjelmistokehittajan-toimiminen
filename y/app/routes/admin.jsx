a59da93cce1500e27f8585ab3e9e91013b6c150c
anssimv <anssi.vilppunen@panic.fi>
2022-09-06 11:07:31 +0300

    kirjatuumisen säätöä

--- y/app/routes/admin.jsx
+++ y/app/routes/admin.jsx
@@ -10,15 +10,13 @@
 
 
 
-
-    function check(form) { /*function to check userid & password*/
-            /*the following code checkes whether the entered userid and password are matching*/
-            if(form.userid.value == "admin" && form.pswrd.value == "admin") {
-                window.open('admin2.html')/*opens the target page while Id & password matches*/
-            }
-            else {
-                alert("Error Password or Username")/*displays error message*/
-            }
+function check(form) { /*function to check userid & password*/
+        /*the following code checkes whether the entered userid and password are matching*/
+        if(form.userid.value == "admin" && form.pswrd.value == "admin") {
+            window.open('admin2.jsx')/*opens the target page while Id & password matches*/
         }
-
+        else {
+            alert("Error Password or Username")/*displays error message*/
+        }
+    }
 
